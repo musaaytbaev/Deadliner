7bc8f425f5b696df527cafcd2fb38b0c05fbc316
Musa <musa_aytbaev@mail.ru>
2016-12-06 10:52:41 +0300

    Start Repository

---
+++ Deadliner.Lib/Repository.cs
@@ -0,0 +1,20 @@
+﻿using Deadliner.Lib.DbModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deadliner.Lib
+{
+    class DeadlineRepository
+    {
+        Context c = new Context();
+
+        public bool Add(Deadline deadline)
+        {
+            c.Deadlines.Add(deadline);
+            return true;
+        }
+    }
+}
