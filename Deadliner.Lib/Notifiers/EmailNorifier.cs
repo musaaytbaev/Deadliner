9376c94b9e7838be95b08112080d501b4ddce8a7
Musa <musa_aytbaev@mail.ru>
2016-12-14 11:38:59 +0300

    Add INotifier and two (while empty) notifier classes: win and email

---
+++ Deadliner.Lib/Notifiers/EmailNorifier.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Deadliner.Lib.DbModel;
+
+namespace Deadliner.Lib.Notifiers
+{
+    class EmailNorifier : INotifier
+    {
+        public Deadline Deadline
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Notify()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Deadliner.Lib/Notifiers/INotifier.cs
@@ -0,0 +1,15 @@
+﻿using Deadliner.Lib.DbModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deadliner.Lib.Notifiers
+{
+    interface INotifier
+    {
+        Deadline Deadline { get; set; }
+        void Notify();
+    }
+}
